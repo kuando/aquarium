a5be0b71405c8e90cc603778ad3773811c4209b9
zgx1993 <273994140@qq.com>
2016-02-23 16:04:45 +0800

    vote-theme

--- public/js/vote.js
+++ public/js/vote.js
@@ -4,13 +4,7 @@
 'use strict'
 
 $(document).ready(function () {
-    //var $container = $('#masonry');
-    //$container.imagesLoaded( function () {
-    //    $container.masonry({
-    //        columnWidth: '.item',
-    //        itemSelector: '.item'
-    //    });
-    //});
+    var $Controls = $(".player-controls");
     //搜索
     $(".vote-menu").on("click","#openSearchModal", function(){
         $("#searchModal").fadeIn()
@@ -20,11 +14,18 @@ $(document).ready(function () {
     });
 
     //帮TA拉票
-    $(".player-controls").on("click","#sharePlayer", function(){
+    $Controls.on("click","#sharePlayer", function(){
         $("#shareModal").fadeIn()
     });
     $(".share-modal").on("click","#closeShare", function(){
         $("#shareModal").fadeOut()
     });
 
+    //关注主办方
+    $Controls.on("click","#followSponsor", function(){
+        $("#followModal").fadeIn()
+    });
+    $(".follow-modal").on("click","#closeFollow", function(){
+        $("#followModal").fadeOut()
+    });
 });
