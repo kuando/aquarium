1bac204ed31d918beac04d057b4efa3e3fcfe103
Frank <kuando@126.com>
2016-03-08 16:35:34 +0800

    微信分享

--- public/js/vote-index.js
+++ public/js/vote-index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     var $container = $('#masonry');
     var $carousel = $('#carousel-example-generic');
-    $carousel.hammer().on('swipeleft', function(){
+    $carousel.hammer().on('swipeleft', function () {
         $(this).carousel('next');
     });
 
-    $carousel.hammer().on('swiperight', function(){
+    $carousel.hammer().on('swiperight', function () {
         $(this).carousel('prev');
     });
 
@@ -28,5 +28,55 @@ $(document).ready(function () {
         onPageClick: function (event, page) {
             $('#page-content').text('Page ' + page);
         }
+    });
+
+    //投票分享
+    var url = location.href;
+    var eventId = $('#eventId').val();
+    var title = $('#shareTitle').text();
+    var shareUrl = new YouAreI(url).query_set({followFlag: 0}).to_string();
+    var coverImage = $("#coverImage").attr('src');
+    $.post("/wx/sdk", {url: url.split('#')[0]}, function (data) {
+        wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: data.appid, // 必填，公众号的唯一标识
+            timestamp: data.timestamp, // 必填，生成签名的时间戳
+            nonceStr: data.nonceStr, // 必填，生成签名的随机串
+            signature: data.signature,// 必填，签名，见附录1
+            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+        wx.ready(function () {
+            wx.onMenuShareTimeline({
+                title: title,
+                link: shareUrl,
+                imgUrl: coverImage,
+                success: function () {
+                    if (eventId && eventId !== '') {
+                        $.ajax({
+                            url: '/events/' + eventId + '/share',
+                            method: 'PUT'
+                        }).then(function () {
+
+                        });
+                    }
+                }
+            });
+            wx.onMenuShareAppMessage({
+                title: title,
+                desc: '快来参加' + title,
+                imgUrl: coverImage,
+                link: shareUrl,
+                success: function () {
+                    if (eventId && eventId !== '') {
+                        $.ajax({
+                            url: '/events/' + eventId + '/share',
+                            method: 'PUT'
+                        }).then(function () {
+
+                        });
+                    }
+                }
+            })
+        });
     });
 });
--- public/js/weixin.js
+++ public/js/weixin.js
@@ -4,7 +4,7 @@
 'use strict';
 var url = location.href;
 var eventId = $('#eventId').val();
-$.get("/wx/sdk?url=" + url.split('#')[0], function (data) {
+$.post("/wx/sdk", {url: url.split('#')[0]}, function (data) {
     url = new YouAreI(url);
     var query = url.query_get();
     var task = query.task;
@@ -15,7 +15,7 @@ $.get("/wx/sdk?url=" + url.split('#')[0], function (data) {
         taskData.task = task;
     }
     wx.config({
-        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: data.appid, // 必填，公众号的唯一标识
         timestamp: data.timestamp, // 必填，生成签名的时间戳
         nonceStr: data.nonceStr, // 必填，生成签名的随机串
@@ -24,9 +24,13 @@ $.get("/wx/sdk?url=" + url.split('#')[0], function (data) {
     });
 
     wx.ready(function () {
+
         wx.onMenuShareTimeline({
+            title: '分享标题',
             link: url.query_set({followFlag: 0}).to_string(),
+
             success: function () {
+                alert('分享成功');
                 if (eventId && eventId !== '') {
                     $.ajax({
                         url: '/events/' + eventId + '/share',
@@ -41,9 +45,13 @@ $.get("/wx/sdk?url=" + url.split('#')[0], function (data) {
 
             }
         });
+
         wx.onMenuShareAppMessage({
+            title: '分享标题',
+            desc: '描述',
             link: url.query_set({followFlag: 0}).to_string(),
             success: function () {
+                alert('发送朋友成功');
                 if (eventId && eventId !== '') {
                     $.ajax({
                         url: '/events/' + eventId + '/share',
@@ -52,6 +60,9 @@ $.get("/wx/sdk?url=" + url.split('#')[0], function (data) {
 
                     });
                 }
+            },
+            cancel: function () {
+
             }
         })
     });
