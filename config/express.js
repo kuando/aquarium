b5893993148e074ec0cf9b82029db7fa339d9c97
Frank <kuando@126.com>
2016-03-09 16:40:40 +0800

    错误处理

--- config/express.js
+++ config/express.js
@@ -33,6 +33,25 @@ requireDir('../models', {
 //初始化 server router
 require('../routes')(app);
 
+app.use(function (err, req, res, next) {
+    console.error(err);
+    let accept = req.accepts('html', 'text', 'json');
+    let status = err.status || 500;
+    switch (accept) {
+        case 'html':
+            return res.render(`error/${status}`);
+        case 'json':
+        case 'text':
+            return res.statusCode(status).send(err.message);
+    }
+    return next();
+});
+
+// 如果没有路由匹配,直接跳转404
+app.use(function (req, res) {
+    res.render('error/404');
+});
+
 module.exports = app;
 
 
