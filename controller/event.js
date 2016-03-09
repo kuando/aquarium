f5de808a6466ea6bcce86c171e9e771ba3e42f7b
zgx1993 <273994140@qq.com>
2016-03-09 17:20:58 +0800

    404 500

--- controller/event.js
+++ controller/event.js
@@ -75,7 +75,6 @@ module.exports = {
     findVideoById: (req, res, next)=> {
         let eventId = req.params.eventId;
         let videoId = req.params.videoId;
-        console.log(videoId);
         Classroom.findOneAndUpdate({
             _id: eventId,
             courses: {
