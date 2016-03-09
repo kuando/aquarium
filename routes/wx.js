4a8d600448dff966ea6b158592b443d0bf9c31f6
Frank <kuando@126.com>
2016-03-09 16:45:52 +0800

    注释第三方托管功能

--- routes/wx.js
+++ routes/wx.js
@@ -4,18 +4,18 @@
 'use strict';
 
 const wx = require('../controller/wx');
-const wxMid = require('../middleware/wx');
+//const wxMid = require('../middleware/wx');
 const sdkMid = require('../middleware/sdk');
 module.exports = function (app) {
 
-    // 接收微信授权事件
-    app.post('/wx/auth/event', wx.authEvent);
-
-    //授权发起页
-    app.get('/wx/auth', wxMid.preAuthCode, wx.authPage);
-
-
-    app.get('/wx/auth/success', wx.authCallback);
+    //// 接收微信授权事件
+    //app.post('/wx/auth/event', wx.authEvent);
+    //
+    ////授权发起页
+    //app.get('/wx/auth', wxMid.preAuthCode, wx.authPage);
+    //
+    //
+    //app.get('/wx/auth/success', wx.authCallback);
 
     //获取jsdk签名
     app.post('/wx/sdk', sdkMid.getSignature, wx.getSdkSignature);
